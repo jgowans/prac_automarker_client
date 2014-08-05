a35121e2623d7320c3c31b6cf14d9b6603616075
James Gowans <gowans.james@gmail.com>
2014-08-05 17:02:59 +0200

    groups changed to be obj orrientated. Directroy assigning written but not tested

--- automarker.py
+++ automarker.py
@@ -5,17 +5,45 @@ import logging
 import csv
 import group_manager
 
+BASE_DIR = "/tmp/Practical1/"
+
 logging.basicConfig(level=logging.INFO, format="%(asctime)s:" + logging.BASIC_FORMAT)
 logging.info("Automarker beginning execution")
 
 #interface = interface_lib.InterrigatorInterface()
 
 groupman = group_manager.GroupManager("/tmp/groups.csv")
-
 # rename all submission folders to only be student numbers
-for old_dir in os.walk("/tmp/Practical1/").send(None)[1]:
-    new_dir = old_dir[old_dir.find('(') + 1 : old_dir.find(')')].upper()
-    os.rename("/tmp/Practical1/" + old_dir, "/tmp/Practical1/" + new_dir)
+#for old_dir in os.walk("/tmp/Practical1/").send(None)[1]:
+#    new_dir = old_dir[old_dir.find('(') + 1 : old_dir.find(')')].upper()
+#    os.rename("/tmp/Practical1/" + old_dir, "/tmp/Practical1/" + new_dir)
+
+# iterate through the folder, assigning a directory to each group
+os.chdir(BASE_DIR)
+sub_dirs = os.listdir()
+groupman.restart()
+while (groupman.has_next() == True):
+    group = groupman.next()
+    group_dir = None
+    for d in sub_dirs:
+        for stdnum in group.members:
+            if d.find(stdnum.lower()) >= 0:
+                if group_dir is None:
+                    group_dir = d
+                else:
+                    group_dir = None
+                    groupman.set_comment(groupnum, "Multiple submissions not marked")
+    group.directory = BASE_DIR + group_dir
+
+
+    #groupman.set_dir(groupnum, 
+        # try match a dir for member 1
+    #    if d.find(stdnums[
+    # try match a dir for member 2
+    # if two dirs defined:
+        # comment = multiple submissions, mark = 0
+    # if no dirs, do nothing
+    # if only one dir, assign to group
 
 # for each group:
 # assert that only one member has submitted.
--- group_manager.py
+++ group_manager.py
@@ -1,13 +1,18 @@
 import csv
 import logging
 
+class Group:
+    def __init__(self, members):
+        self.members = members
+        self.comment = "Undefined"
+        self.directory = None
+        self.mark = -1
+        self.submissiontime = None
+
 class GroupManager:
     def __init__(self, filename):
         logging.info("initialising a groups manager")
-        self.groups = {} # a dict mapping a group number to a list of stdnums
-        self.marks = {} # a dict relating group number to their marks
-        self.comments = {} # a dict relating group number to their comments
-        self.submission_time = {} # group num to submission date time
+        self.groups = [] # a dict mapping a group number to a list of stdnums
         self.group_pointer = 0
         with open(filename) as groupsfile:
             groupsfile_reader = csv.reader(groupsfile, delimiter=',')
@@ -15,11 +20,7 @@ class GroupManager:
                 # prune all empty members, then initialise the marks and comments dicts
                 if group.count('') == 1:
                     group.remove('')
-                self.groups[self.group_pointer] = group
-                self.marks[self.group_pointer] = -1
-                self.comments[self.group_pointer] = "Undefined."
-                self.group_pointer += 1
-        self.group_pointer = 0
+                self.groups.append(Group(group))
         logging.info("group creating completed with a total of {groupnums} groups.".format(groupnums=len(self.groups)))
 
     def has_next(self):
@@ -27,28 +28,15 @@ class GroupManager:
             return True
         return False
 
+    def restart(self):
+        self.group_pointer = 0
+
     def next(self):
         if self.group_pointer == len(self.groups):
             logging.error("tried to acces group after no more groups")
             raise Exception
         self.group_pointer += 1
-        return self.group_pointer - 1
-
-    def get_members(self, groupnum):
-        return self.groups[groupnum]
-
-    def set_mark(self, groupnum, mark):
-        self.marks[group] = mark
-
-    def set_comment(self, groupnum, comment):
-        self.comments[group] = comment
-
-    def set_submission_time(self, groupnum, time):
-        # probably have to do processing to convert Vula timestamp to something more meaningful
-        self.submission_time[groupnum] = time
-
-    def get_submission_time(self, groupnum):
-        return self.submission_time[groupnum]
+        return self.groups[self.group_pointer - 1]
 
     def generate_output():
         pass
